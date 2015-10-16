1d7f9212ca7449c4c9866a0a01fe5f376bf17dda
rgy19930329 <593289488@qq.com>
2015-10-16 17:49:56 +0800

    完成直接插入排序

---
+++ AOAL/直接插入排序算法/js/index.js
@@ -0,0 +1,148 @@
+
+(function(){
+
+Array.prototype.sort2 = function(){
+	var len = this.length;
+	var order = this.slice(0, 1);
+	var unorder = this.slice(1);
+	var res = [];
+	// var obj = {};
+	// obj.order = order.concat();
+	// obj.unorder = unorder.concat();
+	// res.push(obj);
+	// console.log(order, unorder);
+
+	for(var i = 1; i < len; i++){
+		var target = unorder.shift();
+		for(var j = 0; j < order.length; j++){
+			if(target <= order[j]){
+				break;
+			}
+		}
+		order.splice(j, 0, target);
+		console.log(order, unorder);
+		var obj = {};
+		obj.order = order.concat();
+		obj.unorder = unorder.concat();
+		res.push(obj);
+	}
+	return res;
+}
+
+// ----------------------- //
+
+var res = [];
+var rIndex = 0;
+
+build.onmousedown = function(){
+
+	var list = entry.value.split(',');
+	var arr = [];
+	for(var i = 0, len = list.length; i < len; i++){
+		arr[i] = parseInt(list[i]);
+	}
+
+	// ---------- //
+
+	var itemdiv = document.createElement('div');
+
+	var index = 0;
+	var clock = setInterval(function(){
+		if(index >= arr.length){
+			var start = document.createElement('button');
+			start.setAttribute('tag', 'one');
+			start.innerHTML = 'start';
+			datashow.appendChild(start);
+			clearInterval(clock);
+
+			start.onmousedown = nextStep;
+			
+			res = arr.sort2();
+			console.log(res)
+			return;
+		}
+
+		var temp = itemdiv.cloneNode(true);
+		temp.innerHTML = arr[index];
+		datashow.appendChild(temp);
+
+		index++;
+	}, 100);
+
+	entry.disabled = "disabled";
+}
+
+EventUtil.addEvent(document, 'mouseup', function(){
+	var target = EventUtil.getTarget();
+	if(target.tagName.toLowerCase() == 'button' && target.getAttribute('tag') == 'one'){
+		target.disabled = "disabled";
+		CssUtil.setCss(target, {
+			'background': '#eee'
+		});
+	}
+});
+
+function nextStep(){
+	if(rIndex >= res.length){
+		return;
+	}
+	var divList = this.parentNode.getElementsByTagName('div');
+	var order = res[rIndex].order;
+	var unorder = res[rIndex].unorder;
+	var arr = order.concat(unorder);
+	var t = order.length - 1;
+	rIndex++;
+
+	AnimUtil.animate(divList[t], {
+		'background': 'orange',
+		'transform': 'scale(1.2)'
+	}, {
+		'dur': 400
+	}, function(){
+		AnimUtil.animate(divList[t], {
+			'transform': 'scale(1)'
+		}, {
+			'dur': 400
+		});
+	});
+
+	// ---------- //
+	setTimeout(function(){
+
+		var section = document.createElement('section');
+		datashow.appendChild(section);
+
+		var itemdiv = document.createElement('div');
+
+		var index = 0;
+		var clock = setInterval(function(){
+			if(index >= arr.length){
+				var next = document.createElement('button');
+				next.setAttribute('tag', 'one');
+				next.innerHTML = 'next';
+				section.appendChild(next);
+				clearInterval(clock);
+
+				next.onmousedown = nextStep;
+				return;
+			}
+
+			var temp = itemdiv.cloneNode(true);
+			temp.innerHTML = arr[index];
+			section.appendChild(temp);
+
+			if(index <= rIndex){
+				CssUtil.setCss(temp, {
+					'background': 'red'
+				});
+			}
+
+			index++;
+		}, 100);
+
+	}, 1000);
+
+}
+
+
+})();
---
+++ AOAL/直接插入排序算法/js/ktabs.js
@@ -0,0 +1,150 @@
+function Ktabs(tabs){
+
+	window.requestAnimationFrame = (function() {
+		return window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function(callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+	})();
+
+	function toCamel(name){
+		return name.replace(/-[a-z]{1}/g, function(item){
+			return item.slice(1).toUpperCase();
+		});
+	}
+
+	/**
+		css工具
+	*/
+	var CssUtil = {
+		
+		setCss: function(source, obj){
+			if(Object.prototype.toString.call(source) == '[object String]'){
+				var list = document.querySelectorAll(source);
+				arguments.callee(list, obj);
+			}else if(Object.prototype.toString.call(source) == '[object NodeList]' || 
+				Object.prototype.toString.call(source) == '[object HTMLCollection]'){
+				for(var i = 0, len = source.length; i < len; i++){
+					for(var k in obj){
+						source[i].style[toCamel(k)] = obj[k];
+					}
+				}
+			}else{
+				for(var k in obj){
+					source.style[toCamelF(k)] = obj[k];
+				}
+			}
+		},
+
+		hasClass: function(source, value){
+			return source.className.match(new RegExp('(\\s|^)' + value + '(\\s|$)'));
+		},
+
+		addClass: function(source, value){
+			if(!this.hasClass(source, value)){
+				source.className += ' ' + value;
+			}
+		},
+
+		removeClass: function(source, value){
+			if(this.hasClass(source, value)){
+				source.className = source.className.replace(new RegExp('(\\s|^)' + value + '(\\s|$)'), '')
+			}
+		},
+
+		addClassAll: function(source, value){
+			for(var i = 0, len = source.length; i < len; i++){
+				this.addClass(source[i], value);
+			}
+		},
+
+		removeClassAll: function(source, value){
+			for(var i = 0, len = source.length; i < len; i++){
+				this.removeClass(source[i], value);
+			}
+		}
+	}
+
+	/**
+		event工具
+	*/
+	var EventUtil = {
+		preventDefault: function(event) {
+			event = event || window.event;
+			if (event.preventDefault) { //标准浏览器
+				event.preventDefault();
+			} else {
+				event.returnValue = false;
+			}
+		}
+	}
+
+	/**
+		animation工具
+	*/
+	var AnimUtil = {
+		opacity: function(source, dur){
+			var per = 0;
+			var startTime = Date.now();
+
+			requestAnimationFrame(function f() {
+	           if (per >= 1) {
+	               source.style.opacity = 1;// 动画结束
+	           } else {
+	               per = (Date.now() - startTime) / dur;
+
+	               source.style.opacity = per * 1;
+	               requestAnimationFrame(f);
+	           }
+	        });
+		}
+	}
+	
+
+	function init(obj) {
+		var obj = obj || {};
+		var dur = obj.dur || 0;
+		var source = tabs.querySelectorAll('.tabs-nav a');
+		var content = tabs.querySelectorAll('.tabs-content div');
+
+		var fresh = function() {
+			CssUtil.setCss(
+				content, {
+					"display": "none"
+				}
+			);
+
+			CssUtil.setCss(
+				'.tabs-content .content-show', {
+					"display": "block"
+				}
+			);
+		}
+
+		fresh();
+
+		for (var i = 0, len = source.length; i < len; i++) {
+			source[i].addEventListener('click', function(k) {
+				return function() {
+					EventUtil.preventDefault(event);
+					//
+					CssUtil.removeClassAll(source, 'nav-selected');
+					CssUtil.addClass(source[k], 'nav-selected');
+					CssUtil.removeClassAll(content, 'content-show');
+					CssUtil.addClass(content[k], 'content-show');
+					fresh();
+					//
+					AnimUtil.opacity(content[k], dur);
+				}
+			}(i), false);
+		}
+	}
+
+	return {
+		init: init
+	}
+}
