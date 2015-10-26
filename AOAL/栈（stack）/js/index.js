9d7491499c384968fde52ad5fa5b1ab54faf319b
rgy19930329 <593289488@qq.com>
2015-10-26 19:53:12 +0800

    完成了栈

---
+++ AOAL/栈（stack）/js/index.js
@@ -0,0 +1,66 @@
+
+(function(){
+
+// ----------------- //
+
+build.onmousedown = function(){
+
+	var list = entry.value.split(',');
+	var arr = [];
+	for(var i = 0, len = list.length; i < len; i++){
+		arr[i] = list[i];
+	}
+
+	// frontArr = arr.concat();
+
+	// ---------- //
+	var index = 0;
+	var itemdiv = document.createElement('div');
+	var clock = setInterval(function(){
+		if(index >= arr.length){
+			clearInterval(clock);
+			return;
+		}
+
+		var temp = itemdiv.cloneNode(true);
+		temp.innerHTML = arr[index];
+		front.appendChild(temp);
+
+		index++;
+	}, 100);
+}
+
+EventUtil.addEvent(document, 'mouseup', function(){
+	var target = EventUtil.getTarget();
+	if(target.tagName.toLowerCase() == 'button' && target.getAttribute('tag') == 'one'){
+		target.disabled = "disabled";
+		CssUtil.setCss(target, {
+			'background': '#eee'
+		});
+	}
+});
+
+// ----------------- //
+
+var bar = Ktoast(myToast);
+bar.init({
+	'width': '220px',// toast宽度
+	'time': 1000 // toast持续时间
+});
+
+// ----------------- //
+
+pushBtn.onmousedown = function(){
+	var item = front.removeChild(front.lastChild);
+	stack.appendChild(item);
+	bar.emit(item.innerHTML + ' 节点入栈');
+}
+
+popBtn.onmousedown = function(){
+	var node = stack.removeChild(stack.lastChild);
+	rear.appendChild(node);
+	bar.emit(node.innerHTML + ' 节点出栈');
+}
+
+
+})();
