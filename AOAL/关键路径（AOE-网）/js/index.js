5d4075937153417718c3e6cdcd80b6e36ffa46ee
rgy19930329 <593289488@qq.com>
2015-11-01 01:17:43 +0800

    完成了关键路径AOE网

--- AOAL/关键路径（AOE-网）/js/index.js
+++ AOAL/关键路径（AOE-网）/js/index.js
@@ -2,99 +2,79 @@
 (function(){
 
 	var config = {
-		'width': '370px',// 图的宽度
+		'width': '850px',// 图的宽度
 		'height': '290px',// 图的高度
 		'nodeSize': '50px',// 节点尺寸
 		'nodeBgColor': '#59f',// 节点背景颜色
 		'nodeTextColor': '#fff',// 节点文字颜色
 		'arcColor': '#6EB215',// 弧的颜色
 		'hasDirected': true, // 是否是有向图
-		'hasArcWeight': false // 是否显示权值
+		'hasArcWeight': true // 是否显示权值
 	};
 
 	var config_1 = {
-		'opacity': '0'
+		'background': 'orange'
 	};
 
 	var arr = [ 
-		[0, 1, 1, 1, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 1, 0, 0, 1, 0],
-		[0, 0, 0, 0, 1, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 1, 1, 0]
+		[0, 6, 4, 5, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 0, 0, 0, 0],
+		[0, 0, 0, 0, 1, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 2, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 9, 7, 0],
+		[0, 0, 0, 0, 0, 0, 0, 4, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 2],
+		[0, 0, 0, 0, 0, 0, 0, 0, 4],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0]
 	];
 	Kgraph(graph).init(arr, config);
 
-	var arr2 = [ 
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 1, 0, 0, 1, 0],
-		[0, 0, 0, 0, 1, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 1, 1, 0]
-	];
+
 	var tgraph2 = Kgraph(graph2);
-	tgraph2.init(arr2, config);
-	CssUtil.setCss(tgraph2.getAllNodes()[0], config_1);
+	tgraph2.init(arr, config);
+	var list2 = tgraph2.getAllNodes();
+	CssUtil.setCss(list2[0], config_1);
+	CssUtil.setCss(list2[1], config_1);
+	CssUtil.setCss(list2[4], config_1);
+	CssUtil.setCss(list2[6], config_1);
+	CssUtil.setCss(list2[8], config_1);
 
-	var arr3 = [ 
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 1, 0, 0, 1, 0],
-		[0, 0, 0, 0, 1, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0]
-	];
 	var tgraph3 = Kgraph(graph3);
-	Kgraph(graph3).init(arr3, config);
-	CssUtil.setCss(tgraph3.getAllNodes()[0], config_1);
-	CssUtil.setCss(tgraph3.getAllNodes()[5], config_1);
+	Kgraph(graph3).init(arr, config);
+	var list3 = tgraph3.getAllNodes();
+	CssUtil.setCss(list3[0], config_1);
+	CssUtil.setCss(list3[1], config_1);
+	CssUtil.setCss(list3[4], config_1);
+	CssUtil.setCss(list3[7], config_1);
+	CssUtil.setCss(list3[8], config_1);
 
-	var arr4 = [ 
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 1, 0, 0, 1, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0]
-	];
+
 	var tgraph4 = Kgraph(graph4);
-	Kgraph(graph4).init(arr4, config);
-	CssUtil.setCss(tgraph4.getAllNodes()[0], config_1);
-	CssUtil.setCss(tgraph4.getAllNodes()[5], config_1);
-	CssUtil.setCss(tgraph4.getAllNodes()[3], config_1);
+	Kgraph(graph4).init(arr, config);
+	var list4 = tgraph4.getAllNodes();
+	CssUtil.setCss(list4[0], config_1);
+	CssUtil.setCss(list4[2], config_1);
+	CssUtil.setCss(list4[4], config_1);
+	CssUtil.setCss(list4[6], config_1);
+	CssUtil.setCss(list4[8], config_1);
 
-	var arr5 = [ 
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0]
-	];
 	var tgraph5 = Kgraph(graph5);
-	Kgraph(graph5).init(arr5, config);
-	CssUtil.setCss(tgraph5.getAllNodes()[0], config_1);
-	CssUtil.setCss(tgraph5.getAllNodes()[5], config_1);
-	CssUtil.setCss(tgraph5.getAllNodes()[3], config_1);
-	CssUtil.setCss(tgraph5.getAllNodes()[2], config_1);
+	Kgraph(graph5).init(arr, config);
+	var list5 = tgraph5.getAllNodes();
+	CssUtil.setCss(list5[0], config_1);
+	CssUtil.setCss(list5[2], config_1);
+	CssUtil.setCss(list5[4], config_1);
+	CssUtil.setCss(list5[7], config_1);
+	CssUtil.setCss(list5[8], config_1);
 
-	var arr6 = [ 
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0]
-	];
 	var tgraph6 = Kgraph(graph6);
-	Kgraph(graph6).init(arr6, config);
-	CssUtil.setCss(tgraph6.getAllNodes()[0], config_1);
-	CssUtil.setCss(tgraph6.getAllNodes()[5], config_1);
-	CssUtil.setCss(tgraph6.getAllNodes()[3], config_1);
-	CssUtil.setCss(tgraph6.getAllNodes()[2], config_1);
-	CssUtil.setCss(tgraph6.getAllNodes()[1], config_1);
+	Kgraph(graph6).init(arr, config);
+	var list6 = tgraph6.getAllNodes();
+	CssUtil.setCss(list6[0], config_1);
+	CssUtil.setCss(list6[3], config_1);
+	CssUtil.setCss(list6[5], config_1);
+	CssUtil.setCss(list6[7], config_1);
+	CssUtil.setCss(list6[8], config_1);
 
 	// -------------- //
 
@@ -103,30 +83,22 @@
 		EventUtil.addEvent(nextBtns[i], 'click', function(k){
 			return function(){
 				document.body.scrollTop = document.body.scrollHeight;
-				var curli = nextBtns[k].parentNode.parentNode.parentNode;
+				var curli = nextBtns[k].parentNode.parentNode.parentNode.parentNode;
 				var nextli = findNextByTag(curli, 'li');
 				AnimUtil.animate(nextli, {
 					'height': '290px'
+				}, {
+					'easing': 'linear'
 				});
 			}
 		}(i));
 	}
 
-	EventUtil.addEvent(next_sort, 'click', function(){
-		var pnode = sort_result.parentNode;
-		AnimUtil.animate(pnode, {
-			'opacity': '1'
-		}, {
-			'dur': 2000
-		});
-
-		CssUtil.setCss(myTyper, {
-			'display': 'block'
-		});
-		Ktyper(myTyper).init({
-			'dur': 100 // 打印每个字的间隔时间
-		});
+	EventUtil.addEvent(next, 'click', function(){
 		document.body.scrollTop = document.body.scrollHeight;
+		AnimUtil.animate(myfooter, {
+			'opacity': '1'
+		});
 	});
 
 	function findNextByTag(source, tag){
